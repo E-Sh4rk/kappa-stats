4bc7e23c6a33fa08173273ddd7a90038d2938393
Mickaël LAURENT <esh4rk@gmail.com>
2017-06-12 01:33:37 +0200

    update

--- KaTest.ml
+++ KaTest.ml
@@ -15,8 +15,14 @@ let options = [
 ]
 
 let description = ""
-
 let log s = print_string s ; print_newline () ; flush stdout
+
+let rule_ast_name env rule_id = 
+  Format.asprintf "%a" 
+    (Model.print_ast_rule ~env) 
+    (Model.get_rule env rule_id).Primitives.syntactic_rule
+
+let treat_event instantiation mixture = ()
 
 let main () =
   let () =
@@ -35,10 +41,22 @@ let main () =
     let env = Model.of_yojson (Yojson.Basic.Util.member "env" json) in
     let steps = Trace.of_yojson (Yojson.Basic.Util.member "trace" json) in
     let steps_array = Array.of_list steps in
+    let steps_nb = Array.length steps_array in
 
     if !rule_of_interest = "" then ( let () = Format.eprintf "Please specify a rule of interest." in exit 2 )
     else (
-    
+      let mixture = ref (Replay.init_state ~with_connected_components:true) in
+      for i=0 to steps_nb-1 do
+        let step = steps_array.(i) in
+        (
+        match step with
+        | Rule (rule_id, instantiation, infos) when rule_ast_name env rule_id = !rule_of_interest
+        -> treat_event instantiation (!mixture)
+        | _ -> ()
+        ) ;
+        let (new_mixture, _) = Replay.do_step (Model.signatures env) (!mixture) step in
+        mixture := new_mixture
+      done
     )
 
 let () = main ()
